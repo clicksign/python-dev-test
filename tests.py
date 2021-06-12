2ba523f8a2e2d3649864496a4d0dac91ecc83b43
BrunoLanconi <brunolanconi@outlook.com.br>
2021-06-11 21:33:36 -0300

    Setting some variables type verification
    Here I realized that I's not verifying the type inserted into variables, so I decided to do it!

--- tests.py
+++ tests.py
@@ -9,25 +9,32 @@ class FileTests(unittest.TestCase):
     def test_data_file_existence(self):
         entry_value = VARIABLES["data_file_path"]
         expected_value = True
+        self.assertEqual(type(entry_value), str)
         self.assertEqual(os.path.exists(entry_value), expected_value)
 
     def test_file_existence(self):
         entry_value = VARIABLES["test_file_path"]
         expected_value = True
+        self.assertEqual(type(entry_value), str)
         self.assertEqual(os.path.exists(entry_value), expected_value)
 
     def test_number_of_columns(self):
         entry_value = VARIABLES["number_of_columns"]
         expected_value = True
+        self.assertEqual(type(entry_value), int)
         self.assertEqual(number_of_columns_is(entry_value), expected_value)
 
     def test_variables_columns_related(self):
         entry_value_number_of_columns = VARIABLES["number_of_columns"]
         entry_value_expected_header = VARIABLES["expected_header"]
+        self.assertEqual(type(entry_value_number_of_columns), int)
         self.assertEqual(len(entry_value_expected_header), entry_value_number_of_columns)
 
     def test_variables_known_wrong_elements(self):
-        pass
+        entry_value = VARIABLES["known_wrong_elements"]
+        self.assertEqual(type(entry_value), list)
+        for element in entry_value:
+            self.assertEqual(type(element), str)
 
     def test_special_chars(self):
         pass
