37bf878b5962c527051097effdc818c65d8dc76b
BrunoLanconi <brunolanconi@outlook.com.br>
2021-06-15 17:17:29 -0300

    Found a bug regarding database
    Here I'm fixing a bug.
    Apparently I's using the solely database lenght to compare with the remaining data to be analysed which causes the code to always think that there is more and more lines to analyse.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pandas
 import tests
 import unittest
 from services.variables import VARIABLES
-from services.sqlite import sqlite_erase_from, sqlite_get_dataframe_from, sqlite_create_table_on
+from services.sqlite import sqlite_erase_from, sqlite_get_dataframe_from, sqlite_create_table_on, sqlite_table_exists
 from services.clean import clean_and_validate
 
 
@@ -19,14 +19,17 @@ def concatenate_outputs() -> pandas.DataFrame:
     """
     outputs_path = os.path.join(os.getcwd(), "data", "outputs")
     outputs_files = os.listdir(outputs_path)
+    expected_header = VARIABLES["expected_header"]
+    dataframe = pandas.DataFrame([], columns=expected_header)
     dataframes = []
-    outputs_files_exists = len(outputs_files) > 1
+    outputs_files_exists = len(outputs_files) > 0
     if outputs_files_exists:
         for outputs_file in outputs_files:
             outputs_file_path = os.path.join(outputs_path, outputs_file)
             outputs_dataframe = pandas.read_csv(outputs_file_path)
             dataframes.append(outputs_dataframe)
-    return pandas.concat(dataframes)
+        return pandas.concat(dataframes)
+    return dataframe
 
 
 def concatenate_files() -> pandas.DataFrame:
@@ -60,30 +63,39 @@ def concatenate_files() -> pandas.DataFrame:
 
 def process_data_from(dataframe: pandas.DataFrame, periodically: bool, scheduler: sched.scheduler):
     with sqlite3.connect("SQLite_ClickSign.db") as connection:
-        sqlite_dataframe = sqlite_get_dataframe_from(connection, "data")
-    sqlite_dataframe_size = sqlite_dataframe.shape[0]
+        sqlite_data_dataframe = sqlite_get_dataframe_from(connection, "data")
+        config_table_exists = sqlite_table_exists(connection, "config")
+        if not config_table_exists:
+            config_dataframe = pandas.DataFrame([0], columns=["processed rows"])
+            sqlite_create_table_on(config_dataframe, "config")
+        sqlite_config_dataframe = sqlite_get_dataframe_from(connection, "config")
+    processed_data = sqlite_config_dataframe.loc[0]["processed rows"]
     dataframe_size = dataframe.shape[0]
     processing_data_limit = VARIABLES["processing_data_limit"]
-    is_close_to_finish = sqlite_dataframe_size + processing_data_limit >= dataframe_size
+    future_processed_data = processed_data + processing_data_limit
+    is_close_to_finish = future_processed_data >= dataframe_size
     if is_close_to_finish:
-        dataframe_partial = dataframe[sqlite_dataframe_size:]
+        dataframe_partial = dataframe[processed_data:]
         periodically = False
     else:
-        dataframe_partial = dataframe[sqlite_dataframe_size:sqlite_dataframe_size + processing_data_limit]
+        dataframe_partial = dataframe[processed_data:future_processed_data]
     clean_and_validate(dataframe_partial)
     dataframe_outputs = concatenate_outputs()
-    if sqlite_dataframe.empty:
+    if sqlite_data_dataframe.empty:
         dataframe_sqlite = dataframe_outputs
     else:
-        dataframe_sqlite = pandas.concat([sqlite_dataframe, dataframe_outputs])
+        dataframe_sqlite = pandas.concat([sqlite_data_dataframe, dataframe_outputs])
     difference_between_dataframes = pandas.concat([dataframe_sqlite,
-                                                  sqlite_dataframe]).drop_duplicates(keep=False)
-    there_is_no_difference_between_dataframes = difference_between_dataframes.empty
-    if there_is_no_difference_between_dataframes:
-        answer = input("All data seems processed! Are you sure to proceed? (Y/N): ")
-        if answer.lower() in ["n", "no"]:
-            return
-    sqlite_create_table_on(dataframe_sqlite, "data")
+                                                  sqlite_data_dataframe]).drop_duplicates(keep=False)
+    if not dataframe_sqlite.empty:
+        there_is_no_difference_between_dataframes = difference_between_dataframes.empty
+        if there_is_no_difference_between_dataframes:
+            answer = input("All data seems processed! Are you sure to proceed? (Y/N): ")
+            if answer.lower() in ["n", "no"]:
+                return
+        sqlite_create_table_on(dataframe_sqlite, "data")
+    config_dataframe = pandas.DataFrame([future_processed_data], columns=["processed rows"])
+    sqlite_create_table_on(config_dataframe, "config")
     if periodically:
         run_every_seconds = VARIABLES["run_every_seconds"]
         scheduler.enter(run_every_seconds, 1, process_data_from, (dataframe, periodically, scheduler,))
@@ -92,33 +104,41 @@ def process_data_from(dataframe: pandas.DataFrame, periodically: bool, scheduler
 def main():
     try:
         action = sys.argv[1]
+        try:
+            additional_action = sys.argv[2]
+        except IndexError:
+            additional_action = None
         print("Data processing started. Use CTRL^C to stop!")
         if action in ["-t", "--test", ]:
-            suite = unittest.TestLoader().loadTestsFromModule(tests)
-            unittest.TextTestRunner(verbosity=2).run(suite)
-        elif action in ["-s", "--start", "-p", "--proceed", "-ot", "--one-time", ]:
+            principal_suite = unittest.TestLoader().loadTestsFromModule(tests)
+            unittest.TextTestRunner(verbosity=2).run(principal_suite)
+        elif action in ["-s", "--start", "-p", "--proceed", ]:
             if action in ["-s", "--start", ]:
                 sqlite_erase_from("data")
+                config_dataframe = pandas.DataFrame([0], columns=["processed rows"])
+                sqlite_erase_from("config", config_dataframe)
+
+
             dataframe = concatenate_files()
             scheduler = sched.scheduler(time.time, time.sleep)
-            if action in ["-s", "--start", "-p", "--proceed", ]:
-                scheduler.enter(0, 1, process_data_from, (dataframe, True, scheduler,))
-            else:
+            if additional_action in ["-ot", "--one-time", ]:
                 scheduler.enter(0, 1, process_data_from, (dataframe, False, scheduler,))
+            else:
+                scheduler.enter(0, 1, process_data_from, (dataframe, True, scheduler,))
             scheduler.run()
         else:
             raise IndexError
     except IndexError:
         print("SyntaxError: This is NOT a valid syntax.")
         print("Please use the following:")
-        print("-t | --test        Tests variables and other functions to process data")
-        print("-s | --start       Start from scratch")
-        print("-p | --proceed     Continue where you left off")
-        print("-ot | --one-time   Continue where you left off just once")
-        print("main.py [-t | --test | -s | --start | -p | --proceed | -ot | --one-time]")
+        print("-t  | --test       Tests variables and other functions to process data")
+        print("-s  | --start      Start from scratch")
+        print("-p  | --proceed    Continue where you left off")
+        print("-ot | --one-time   Run just once")
+        print("main.py <-t | --test | -s | --start | -p | --proceed> [-ot | --one-time]")
     except KeyboardInterrupt:
         print("Data processing stopped with CTRL^C.")
-        print("Use [-p | --proceed] to continue where you left off!")
+        print("Use <-p | --proceed> to continue where you left off!")
 
 
 if __name__ == '__main__':
--- services/sqlite.py
+++ services/sqlite.py
@@ -1,6 +1,7 @@
 import sqlite3
 import pandas
 from sqlalchemy import create_engine
+from .variables import VARIABLES
 
 
 def sqlite_create_table_on(dataframe: pandas.DataFrame, table: str):
@@ -29,12 +30,26 @@ def sqlite_get_dataframe_from(connection: sqlite3.Connection, table: str) -> pan
     return dataframe
 
 
-def sqlite_erase_from(table: str):
+def sqlite_erase_from(table: str, starting_dataframe=None):
     """
     Erases SQLite {table} in SQLite_ClickSign.db
     @type table: str
+    @type starting_dataframe: any
     @param table: a string representing the table to erase
+    @param starting_dataframe: a optional dataframe representing the starting table content
     """
-    dataframe = pandas.DataFrame([], columns=["None"])
+    if starting_dataframe is not None:
+        dataframe = starting_dataframe
+    else:
+        expected_header = VARIABLES["expected_header"]
+        dataframe = pandas.DataFrame([], columns=expected_header)
     engine = create_engine(f"sqlite:///SQLite_ClickSign.db")
     dataframe.to_sql(table, engine, if_exists='replace', index=False)
+
+
+def sqlite_table_exists(connection: sqlite3.Connection, table: str) -> bool:
+    try:
+        pandas.read_sql_query(f"SELECT * FROM {table}", connection)
+    except pandas.io.sql.DatabaseError:
+        return False
+    return True
