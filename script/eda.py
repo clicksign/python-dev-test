8d415135c51743fae6aedaf56708c1fd4426f0e2
Leticia <leticia.ofigueiredo@gmail.com>
2022-05-14 11:29:27 -0300

    EDA OK - Create sweetviz analysis. Optional choise

---
+++ script/eda.py
@@ -0,0 +1,14 @@
+import sweetviz as sv
+import os
+
+def run_exploratory_data_analysis(data, name):
+    """
+    Runs a exploratory data analysis on the dataset.
+    """
+
+    #create folder if not exists
+    if not os.path.exists('../EDA'):
+        os.makedirs('../EDA')
+
+    data_report = sv.analyze(data, pairwise_analysis="on")
+    data_report.show_html(filepath=f"../EDA/{name}.html", open_browser=False)
--- script/pipeline.py
+++ script/pipeline.py
@@ -1,20 +1,27 @@
 import sys, pickle, os
-from extract import load_adult_datasets
+from script.extract import load_adult_datasets
 from script.load import insert_data
-from transform import handle_missing_data
-from load import insert_data
+from script.transform import handle_missing_data
+from script.load import insert_data
+from script.eda import run_exploratory_data_analysis
 
 num_lines_to_process = 1630
 
 # Optionals for running the pipeline
-# run_eda = False
+run_eda = True
 # get_insights = True
 
-def main (auto = False):
+def pipeline_main (auto = False):
+    chunk_read = 0
+    if os.path.exists('chunk_read.pkl'):
+        chunk_read = pickle.load(open('chunk_read.pkl', 'rb'))
+
+        print("Chunks read: ", chunk_read)
+
     # ETL Phase 1 - Extract >> Get datasets 
     adult_data, adult_test = load_adult_datasets(
         first_n_lines = None if not auto else num_lines_to_process,
-        skip_lines = num_lines_to_process
+        skip_lines = chunk_read * num_lines_to_process
     )
 
     # ELT Phase 2 - Transform >> Normalize data types and handle nulls
@@ -24,9 +31,13 @@ def main (auto = False):
         # Handle missing data
         dataset = handle_missing_data(dataset)
 
+        # >> Optional << Run exploratory data analysis
+        if run_eda and not auto:
+            run_exploratory_data_analysis(dataset, name)
+
     # ETL Phase 3 - Load >> Load datasets into database
     insert_data("adult_data", adult_data)
 
 if __name__ == '__main__':
     # Check if script is being executed by crontab
-    main('periodic' in sys.argv)
+    pipeline_main('periodic' in sys.argv)
