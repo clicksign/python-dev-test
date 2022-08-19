58330dab8cece93980aa6816c2375f9fca486c63
“Bereoff” <bbereoff@gmail.com>
2022-08-19 02:35:05 -0300

    Payload structure prepared

--- data/etl.py
+++ data/etl.py
@@ -3,37 +3,49 @@ import os
 import re
 
 from .functions import *
-
-with open("Description", 'r') as f:
-    text = f.read().splitlines()
-
-columns = []
-for sentences in text:
-    lines = map(normalizer, re.findall('^\s*\D[^:]+:\s*', sentences))
-    for line in lines:
-        if line not in columns:
-            columns.append(line)
-        else:
-            continue
-length = len(columns)
-columns = columns[:length-2]
-
-PATH = os.getcwd() + '/Adult.data'
-STEPS = 1630
-NAMES = columns
+from .send_data import send_data
 
 
-length = len(columns)
+def handle_file():
+    with open("Description", 'r') as f:
+        text = f.read().splitlines()
 
-columns = columns[:length-2]
+    columns = []
+    for sentences in text:
+        lines = map(normalizer, re.findall('^\s*\D[^:]+:\s*', sentences))
+        for line in lines:
+            if line not in columns:
+                columns.append(line.replace('-', '_'))
+            else:
+                continue
+    length = len(columns)
+    columns = columns[:length-2]
 
-counter = get_counter()
+    PATH = os.getcwd() + '/Adult.data'
+    STEPS = 1630
+    NAMES = columns
 
-data = get_data(
-    path=PATH,
-    steps=STEPS,
-    counter=counter,
-    names=NAMES
-)
+    length = len(columns)
 
-data_batch(data)
+    columns = columns[:length-2]
+
+    counter = get_counter()
+
+    data = get_data(
+        path=PATH,
+        steps=STEPS,
+        counter=counter,
+        names=NAMES
+    )
+
+    data_batch(data)
+
+    send_data(data.to_dict(orient='records'))
+
+
+def main():
+    handle_file()
+
+
+if __name__ == '__main__':
+    main()
