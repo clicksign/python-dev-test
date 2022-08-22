04283e4c14f485c9b2b0b412a59c965c1ce965da
“Bereoff” <bbereoff@gmail.com>
2022-08-21 21:54:34 -0300

    etl routine file updated

--- data/etl.py
+++ data/etl.py
@@ -1,17 +1,18 @@
 import os
 
-from .functions import *
-from .send_data import send_data
+from functions import *
+from send_data import send_data
 
 
 def handle_file():
-    description_file = os.getcwd() + '/Description'
-    path = os.getcwd() + '/Adult.data'
-    steps = 1630
 
-    text = read_file(description_file)
-    names = generate_column_name(text)
-    dtypes = generate_column_type(text)
+    description_file = os.getcwd() + '/data/Description'
+    file_path = os.getcwd() + '/data/Adult.data'
+    steps = 10
+
+    text = read_description_file(description_file=description_file)
+    names = generate_column_name(text=text)
+    dtypes = generate_column_type(text=text)
 
     data = get_data(
         file_path=file_path,
@@ -20,11 +21,11 @@ def handle_file():
         dtypes=dtypes
     )
 
-    get_removed_data(data)
+    get_removed_data(df=data)
 
-    checkpoint_batch(data)
+    checkpoint_batch(df=data)
 
-    send_data(data.to_dict(orient='records'))
+    send_data(df=data)
 
 
 def main():
