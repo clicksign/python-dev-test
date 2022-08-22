9e6ad9d6987cd0afb7960b88d24d882c09e84c3a
“Bereoff” <bbereoff@gmail.com>
2022-08-22 13:38:51 -0300

    variables updated

--- data/etl.py
+++ data/etl.py
@@ -6,14 +6,15 @@ from send_data import send_data
 
 def handle_file():
 
-    description_file = os.getcwd() + '/data/Description'
-    file_path = os.getcwd() + '/data/Adult.data'
+    FILE_PATH = '/home/bereoff/repos/python-dev-test/data/Adult.data'
+    DESCRIPTION_PATH = '/home/bereoff/repos/python-dev-test/data/Description'
+
     steps = 5
-    text = read_description_file(description_file=description_file)
+    text = read_description_file(description_file=DESCRIPTION_PATH)
     names = generate_column_name(text=text)
     dtypes = generate_column_type(text=text)
     data = get_data(
-        file_path=file_path,
+        file_path=FILE_PATH,
         steps=steps,
         names=names,
         dtypes=dtypes
--- python-dev-cron/handler.py
+++ python-dev-cron/handler.py
@@ -1,6 +1,10 @@
 import datetime
 import logging
 
+from functions import *
+from send_data import *
+from send_counter import *
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -9,3 +13,20 @@ def run(event, context):
     current_time = datetime.datetime.now().time()
     name = context.function_name
     logger.info("Your cron function " + name + " ran at " + str(current_time))
+
+    FILE_PATH = '/home/bereoff/repos/python-dev-test/data/Adult.data'
+    DESCRIPTION_PATH = '/home/bereoff/repos/python-dev-test/data/Description'
+
+    steps = 5
+    text = read_description_file(description_file=DESCRIPTION_PATH)
+    names = generate_column_name(text=text)
+    dtypes = generate_column_type(text=text)
+    data = get_data(
+        file_path=FILE_PATH,
+        steps=steps,
+        names=names,
+        dtypes=dtypes
+    )
+
+    checkpoint_batch(df=data, steps=steps)
+    send_data(df=data)
